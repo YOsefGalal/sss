66d273b50a9c6b0d88645b282723f3027704ce9c
YOsefGalal <45180349+YOsefGalal@users.noreply.github.com>
2018-12-12 17:35:03 +0200

    Add files via upload

---
+++ dndn.js
@@ -0,0 +1,21 @@
+﻿﻿const Discord = require("discord.js");
+const client = new Discord.Client();
+client.on('ready', () => {
+console.log("Discord Bot By So.?");
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+  console.log('BySo.?')
+
+
+});
+
+client.on("guildMemberAdd", member => {
+  member.createDM().then(function (author) {
+  return author.send(' ** حرام واحد جميل مثلك م يكون عندنا .. تنورنا  https://discord.gg/Dq5abcr ** ')
+
+}).catch(console.error)
+})
+
+client.login("NTIxNzc4MjQwMzI3NTE2MTYz.DvK0fg.nVKkyAvco7byGtSKFysQ6RUpqaM");
